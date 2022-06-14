ff5deece8bcd6fd95abebbed295519b88a5553b0
Boloblux <florquo91@gmail.com>
2022-06-14 16:07:29 +0200

    Exo2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun calc(view: View) {
+        var number1: Int = if (number1Input.text.toString().isEmpty()) 0 else number1Input.text.toString().toInt()
+        var number2: Int = if (number2Input.text.toString().isEmpty()) 0 else number2Input.text.toString().toInt()
+        var result: Int? = null
+
+        if (number1 == 0 && number2 == 0) {
+            Toast.makeText(applicationContext,"Veuillez rentrer des valeurs",Toast.LENGTH_LONG).show()
+        }
+        else {
+            if(view.id == R.id.plus) {
+                result = number1 + number2
+            }
+            if(view.id == R.id.minus) {
+                result = number1 - number2
+            }
+            if(view.id == R.id.divide) {
+                if (number2 == 0) {
+                    Toast.makeText(applicationContext,"On ne peut pas diviser par rien",Toast.LENGTH_LONG).show()
+                    result = 0
+                }
+                else {
+                    result = number1 / number2
+                }
+            }
+            if(view.id == R.id.multiply) {
+                result = number1 * number2
+            }
+            resultView.setText(result.toString())
+        }
+
+
     }
 }
